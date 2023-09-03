4072fdf23664bf9631dec5954967ab2ab926073f
pck <3500146682@qq.com>
2023-09-03 21:33:56 +0800

    添加四个路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router-dom";
 import {ErrorPage} from "./components/ErroPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>root</div>,
-    errorElement: <ErrorPage />
+    element: <div>root <Outlet /></div>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <div>index</div>
+      },
+      {
+        path: 'welcome',
+        element: <Outlet />,
+        children: [
+          {
+            index: true,
+            element: <div>index</div>
+          },
+          {
+            path: '1',
+            element: (
+                <div> 1 <NavLink to='/welcome/2'>下一页</NavLink> </div>
+            )
+          },
+          {
+            path: '2',
+            element: (
+                <div> 2 <NavLink to='/welcome/3'>下一页</NavLink> </div>
+            )
+          },
+          {
+            path: '3',
+            element: (
+                <div> 3 <NavLink to='/welcome/4'>下一页</NavLink> </div>
+            )
+          },
+          {
+            path: '4',
+            element: (
+                <div> 4 <NavLink to='/welcome/xxx'>下一页</NavLink> </div>
+            )
+          }
+        ]
+      }
+    ]
   },
 ])
 
