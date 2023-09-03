05766f5118f2951aacc7aae26d58ac6c2da8fefb
pck <3500146682@qq.com>
2023-09-03 16:14:02 +0800

    初始化 router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>root</div>,
+  },
+])
 
 const div = document.getElementById('root') as HTMLElement
 
@@ -8,6 +15,6 @@ const root = ReactDOM.createRoot(div)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
