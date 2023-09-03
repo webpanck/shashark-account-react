080a79580a990b4be9b9047ddede0918f20d2e12
pck <3500146682@qq.com>
2023-09-03 16:45:12 +0800

    创建 404 错误处理页面

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {ErrorPage} from "./components/ErroPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>root</div>,
+    errorElement: <ErrorPage />
   },
 ])
 
